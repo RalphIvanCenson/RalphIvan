393776dd711cfc42b99e7aa4e282190fac4b35d3
RalphIvanCenson <ralphivan2005@gmail.com>
2025-12-10 00:09:27 -0800

    Add Render domain to ALLOWED_HOSTS

--- Ralph/settings.py
+++ Ralph/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-33)c4jbiklrikd1uf@bli
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['ralphivan-4.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
